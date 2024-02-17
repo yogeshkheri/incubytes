7be67360621acf7e74a7a9409252c8fa3806c16b
Yogeshvce <yogesh@highperflow.com>
2024-02-17 11:40:37 +0530

    calculator class with empty string input

--- src/main/java/com/proj/Calculator.java
+++ src/main/java/com/proj/Calculator.java
@@ -1,4 +1,7 @@
 package com.proj;
 
 public class Calculator {
+    public static int add(String text) {
+        return 0;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,7 @@
 import com.proj.Calculator;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
